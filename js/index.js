7cca3ea6508bf17386c8ee3979e20930f846b47e
rhuaannn <rhuanmarques1223@gmail.com>
2023-08-12 17:04:42 -0300

    feat: finish project rocket

---
+++ js/index.js
@@ -0,0 +1,13 @@
+import { Router } from "./router.js";
+
+const router = new Router();
+
+router.add("/", "pages/home.html");
+router.add("/about-universe", "/pages/about-universe.html");
+router.add("/exploration", "/pages/exploration.html");
+router.add(404, "/pages/404.html");
+
+router.handle();
+
+window.onpopstate = () => router.handle();
+window.route = () => router.route();
